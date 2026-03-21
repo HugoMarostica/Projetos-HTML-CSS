dfa3489c404d1e507c4b9faa4031d7be22b56e43
Hugo M. Pimenta <hugomarostica8a@gmail.com>
2026-03-20 22:08:26 -0300

    primeiro commit

---
+++ Light-dark/index.js
@@ -0,0 +1,13 @@
+function trocarTema() {
+
+  const body = document.body;
+
+  if (body.classList.contains("light")) {
+    body.classList.remove("light");
+    body.classList.add("dark");
+  } else {
+    body.classList.remove("dark");
+    body.classList.add("light");
+  }
+
+}
